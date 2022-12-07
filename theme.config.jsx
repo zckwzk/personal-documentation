fcf301c80ebada9ddb60c6e65bf5cf328d38ae7c
zakariaw <zakaria.wicaksono@gmail.com>
2022-12-07 09:41:57 +0700

    Initial Update

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+    logo: <span>zckwzk documentation</span>,
+    project: {
+      link: 'https://github.com/shuding/nextra',
+    },
+    footer: {
+      text: 'Nextra Docs Template',
+    },
+    // ...
+  }
